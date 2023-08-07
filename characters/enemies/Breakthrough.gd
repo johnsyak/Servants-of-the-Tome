318f60efdba34242ad8a502c4b4c9cdffb698cf7
JohnY <johnyak381@gmail.com>
2023-08-06 22:04:07 -0400

    wip

---
+++ characters/enemies/Breakthrough.gd
@@ -0,0 +1,18 @@
+extends State
+
+class_name Breakthrough
+
+@export var enemy: CharacterBody2D
+
+func enter():
+	pass
+	
+func exit():
+	pass
+
+func update(_delta: float):
+	pass
+
+func physics_update(_delta: float):
+	pass
+
